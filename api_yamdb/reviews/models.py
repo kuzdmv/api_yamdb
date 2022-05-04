e51336b225f72056f2edd949b813ccc7af452b83
Кузнецов Дмитрий <kuznetsov@logwest.ru>
2022-05-04 17:10:39 +0300

    created models title, gener, category

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
-# Create your models here.
+from .validators import validate_year
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    yaer = models.IntegerField(validators=[validate_year])
+    description = models.TextField(blank=True)
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='category',
+        blank=True,
+        null=True,
+    )
+    genre = models.ManyToManyField(Genre, through='GenreTitle')
+
+    def __str__(self):
+        return self.name
+
+
+class GenreTitle(models.Model):
+    gener = models.ForeignKey(Genre, on_delete=models.CASCADE)
+    title = models.ForeignKey(Title, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.title} {self.gener}'
